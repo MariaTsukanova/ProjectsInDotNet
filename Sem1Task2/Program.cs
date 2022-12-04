7d29a30989b52688d4aabb60a74da6a0e10662e8
Maria <o.esipova1975@yandex.ru>
2022-12-05 01:55:01 +0300

    added Seminar 1 Task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(num1Line!=null&&num2Line!=null)
+{
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Сравниваем числа и выводим результат в случае, когда первое число больше второго
+    if(num1 > num2)
+    {
+        Console.WriteLine(num1 + " больше " + num2);
+    }
+    //Выводим результат в случае, когда второе число больше первого
+    else
+    {
+        Console.WriteLine(num2 + " больше " + num1);
+    }
+}
+
