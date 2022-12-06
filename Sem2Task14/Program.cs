f7fa36009df2a137cfde5a86e1917244797141fe
Maria <o.esipova1975@yandex.ru>
2022-12-06 15:09:34 +0300

    added Seminar 2 Task 14

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = int.Parse(Console.ReadLine() ?? "0");
+
+int result1 = number % 7;
+int result2 = number % 23;
+
+if ((result1 == 0) && (result2 == 0))
+{
+    Console.WriteLine("число кратно одновременно 7 и 23");
+}
+else
+{
+    Console.WriteLine("число не кратно одновременно 7 и 23");
+}
