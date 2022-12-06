6841863bfb908198f40b6dd0b0343a7c2dfd2b10
Maria <o.esipova1975@yandex.ru>
2022-12-06 14:55:46 +0300

    added Seminar 2 Task 11

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Random numberSintezator = new System.Random();
+
+int num = numberSintezator.Next(100, 1000);
+
+Console.WriteLine(num);
+
+//int firstNumber = num/100;
+//int thirdNumber = num%10;
+
+//int result = firstNumber*10 + thirdNumber;
+
+//int result = (num/100)*10 + num%10;
+
+Console.WriteLine((num/100)*10 + num%10);
+
