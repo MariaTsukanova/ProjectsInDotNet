cc4edcdd68ca0abb36f48aac748355e529e87a17
Maria <o.esipova1975@yandex.ru>
2022-12-11 19:02:40 +0300

    added Seminar 3 Task 21 changes

--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -8,7 +8,7 @@ int Y2 = ReadData("Введите координату Y2");
 int Z2 = ReadData("Введите координату Z2");
 
 
-PrintData("Расстояние между точками: " , DistanceBetweenPoints(X1, Y1, Z1, X2, Y2, Z2));
+PrintData("Расстояние между точками: " , CalculateDistance(X1, Y1, Z1, X2, Y2, Z2));
 
 // Метод читает данные от пользователя
 int ReadData(string msg)
@@ -18,7 +18,7 @@ int ReadData(string msg)
 }
 
 // метод находит расстояние между точками
-double DistanceBetweenPoints(double x1, double x2, double y1, double y2, double z1, double z2)
+double CalculateDistance(double x1, double x2, double y1, double y2, double z1, double z2)
 {
     double distance = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
     return distance;
